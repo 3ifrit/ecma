e814c7651f032fbcbac66cf526c85c63204ab469
Abdelfettah <abdelfettah.youbi@gmail.com>
2022-02-15 19:52:16 +0100

    Il manque le holidays à fixer

--- index.js
+++ index.js
@@ -5,72 +5,66 @@ import axios from 'axios';
 const app = fastify({ logger: true });
 
 let requestCat = async () => {
-  try {
-
-    const cat = await axios.get(`https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=3`);
-    const fact = cat.data.map((cat) => {
-      return cat.text;
-    });
-    return fact;
-
-  } catch (err) {
-    console.log("Error");
-    return null;
-  }
-
-}
-
+    try {
+        const cat = await axios.get(
+            `https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=3`
+        );
+        console.log(cat);
+        const fact = cat.data.map((cat) => {
+            return cat.text;
+        });
+        return fact;
+    } catch (err) {
+        console.log('Error');
+        return null;
+    }
+};
 
 let requestFox = async () => {
+    try {
+        const fox = await axios.post('https://randomfox.ca/floof/');
+        return fox.data.image;
+    } catch (err) {
+        console.log('Error');
+        return null;
+    }
+};
 
-  try {
-    const fox = await axios.get('https://randomfox.ca/floof/');
-    return fox.data.image;
-  } catch (err) {
-    console.log("Error");
-    return null;
-  }
+let requestHolidays = async () => {
+    try {
+        const holidays = await axios.get(
+            `https://date.nager.at/api/v2/PublicHolidays/2021/fr`
+        );
+        return holidays.data;
+    } catch (err) {
+        console.log('Error');
+        return null;
+    }
+};
 
-}
+let fetchApis = async () => {
+    const catFacts = await requestCat();
+    const foxPicture = await requestFox();
+    const holidays = await requestHolidays();
 
-
-let requestHolidays = async (countryCode = 'FR') => {
-
-  try {
-    const holidays = await axios.get(`https://date.nager.at/api/v2/PublicHolidays/2021/${countryCode}`);
-    return holidays.data;
-  } catch (err) {
-    console.log("Error");
-    return null;
-  }
-}
-
-let fetchApis = async (countryCode) =>{
-  const catFacts = await requestCat();
-  const foxPicture = await requestFox();
-  const holidays = await requestHolidays(countryCode);
-
-  return {
-    foxPicture: foxPicture,
-    catFacts: catFacts,
-    holidays: holidays
-  }
-
-}
+    return {
+        foxPicture: foxPicture,
+        catFacts: catFacts,
+        holidays: holidays,
+    };
+};
 
 app.post('/', async (req, res) => {
-
-  return await fetchApis(req.body.countryCode);
-
+    return await fetchApis(req.body);
 });
 
 // Run the server!
 const start = async () => {
-  try {
-    await app.listen(5000);
-  } catch (err) {
-    app.log.error(err);
-    process.exit(1);
-  }
+    try {
+        await app.listen(5000);
+    } catch (err) {
+        app.log.error(err);
+        process.exit(1);
+    }
 };
 start();
